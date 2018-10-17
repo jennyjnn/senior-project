32a02b40f566d7be9d75bafa09b0585935e08920
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-10-18 00:03:40 +0700

    Update xml of register activity

--- MedicationReminder/app/src/main/java/com/jenny/medicationreminder/RegisterActivity.java
+++ MedicationReminder/app/src/main/java/com/jenny/medicationreminder/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.jenny.medicationreminder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        
     }
 }
