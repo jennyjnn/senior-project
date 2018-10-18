4e9c4e4f1dedfb5ad8571179c5605ae1cf9c3613
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-10-19 00:03:54 +0700

    Create disease dialog in register activity

--- MedicationReminder/app/src/main/java/com/jenny/medicationreminder/RegisterActivity.java
+++ MedicationReminder/app/src/main/java/com/jenny/medicationreminder/RegisterActivity.java
@@ -1,16 +1,75 @@
 package com.jenny.medicationreminder;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import mehdi.sakout.fancybuttons.FancyButton;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText etDisease;
+    FancyButton btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        etDisease = findViewById(R.id.etDisease);
+        btnCancel = findViewById(R.id.btnCancel);
+
+        etDisease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                final String [] diseaseArr = {"โรคเกาต์", "โรคเบาหวาน", "โรคไขมันในเลือดสูง", "โรคความดันโลหิตสูง", "โรคมะเร็งต่อมลูกหมาก", "โรคไต", "โรคหัวใจขาดเลือด", "โรคจอประสาทตาเสื่อม", "โรคอัลไซเมอร์"};
+
+                final boolean[] checkedDisease = new boolean[9];
+                final List<String> diseaseList = Arrays.asList(diseaseArr);
+
+                builder.setTitle("โรคประจำตัว");
+
+                builder.setMultiChoiceItems(diseaseArr, checkedDisease, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        checkedDisease[which] = isChecked;
+                        String currentItem = diseaseList.get(which);
+                    }
+                });
+
+                builder.setPositiveButton("ยืนยัน", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String disease = "";
+                        for (int i=0; i<checkedDisease.length; i++){
+                            boolean checked = checkedDisease[i];
+                            if (checked){
+                                disease += diseaseList.get(i) + ", ";
+                            }
+                        }
+                        etDisease.setText(disease.substring(0, disease.length()-2));
+
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
 }
