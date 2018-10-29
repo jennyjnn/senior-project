7d0acf64e80e50617cb57b18de91a40e83efdbbf
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-10-29 13:22:11 +0700

    Edit id of view

--- MedicationReminder/app/src/main/java/com/jenny/medicationreminder/LoginTelActivity.java
+++ MedicationReminder/app/src/main/java/com/jenny/medicationreminder/LoginTelActivity.java
@@ -1,11 +1,27 @@
 package com.jenny.medicationreminder;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginTelActivity extends AppCompatActivity {
+
+    EditText etLoginTel;
+
+    FirebaseDatabase database;
+    DatabaseReference loginTelRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +37,31 @@ public class LoginTelActivity extends AppCompatActivity {
         Intent intent = new Intent(LoginTelActivity.this, RegisterActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void loginTel(View view) {
+        etLoginTel = findViewById(R.id.etLoginTel);
+
+        database = FirebaseDatabase.getInstance();
+        loginTelRef = database.getReference("User");
+
+//        loginTelRef.orderByChild("User_phone").equalTo(String.valueOf(etLoginTel)).addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                String data = dataSnapshot.getValue(String.class);
+//                if (count == 0) {
+//                    Toast.makeText(LoginTelActivity.this, "Error: incorrect phone number.", Toast.LENGTH_LONG).show();
+//                } else {
+//                    Toast.makeText(LoginTelActivity.this, "Login success", Toast.LENGTH_LONG).show();
+//                }
+//                Log.e("data", data);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+
     }
 }
