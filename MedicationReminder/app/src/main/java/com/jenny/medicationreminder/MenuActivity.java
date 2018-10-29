af2654d986a8adb7c9e5fa81d386f21df48cbdc4
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-10-30 01:39:07 +0700

    Create gradient color for app bar

--- MedicationReminder/app/src/main/java/com/jenny/medicationreminder/MenuActivity.java
+++ MedicationReminder/app/src/main/java/com/jenny/medicationreminder/MenuActivity.java
@@ -2,12 +2,23 @@ package com.jenny.medicationreminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 public class MenuActivity extends AppCompatActivity {
+
+    CardView cvAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        initInstances();
+
+    }
+
+    private void initInstances() {
+        cvAppBar = findViewById(R.id.cvAppBar);
+        cvAppBar.setBackgroundResource(R.drawable.bg_appbar);
     }
 }
