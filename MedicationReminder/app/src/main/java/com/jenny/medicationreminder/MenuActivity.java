28892e711aa40ba2b75e2e7cb739b78b01a669a3
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-10-30 01:56:09 +0700

    Create alert dialog for logout

--- MedicationReminder/app/src/main/java/com/jenny/medicationreminder/MenuActivity.java
+++ MedicationReminder/app/src/main/java/com/jenny/medicationreminder/MenuActivity.java
@@ -1,8 +1,11 @@
 package com.jenny.medicationreminder;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -20,5 +23,26 @@ public class MenuActivity extends AppCompatActivity {
     private void initInstances() {
         cvAppBar = findViewById(R.id.cvAppBar);
         cvAppBar.setBackgroundResource(R.drawable.bg_appbar);
+    }
+
+    public void logOut(View view) {
+        AlertDialog.Builder alertLogout = new AlertDialog.Builder(this);
+        alertLogout.setMessage("คุณต้องการออกจากระบบใช่หรือไม่ ?");
+        alertLogout.setPositiveButton("ใช่", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        alertLogout.setNeutralButton("ไม่ใช่", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog alertDialog = alertLogout.create();
+        alertDialog.show();
+
     }
 }
