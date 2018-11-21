e208f8997a0842b0ec3b22cc262b03a7592ff1d8
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-11-22 04:10:00 +0700

    Create view profile activity

---
+++ MedicationReminder/app/src/main/java/com/jenny/medicationreminder/ViewProfileActivity.java
@@ -0,0 +1,28 @@
+package com.jenny.medicationreminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+public class ViewProfileActivity extends AppCompatActivity {
+    CardView cvAppBarViewProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        // set color for app bar
+        cvAppBarViewProfile = findViewById(R.id.cvAppBarViewProfile);
+        cvAppBarViewProfile.setBackgroundResource(R.drawable.bg_appbar);
+    }
+
+    public void backBtn(View view) {
+        finish();
+    }
+}
