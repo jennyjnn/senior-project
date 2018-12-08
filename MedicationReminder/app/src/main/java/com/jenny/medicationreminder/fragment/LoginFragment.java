833f4c4fabf2a2c537b45649dc10345f214ed5f6
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-12-08 19:08:14 +0700

    Change Login Activity to Login Fragment and change UI

---
+++ MedicationReminder/app/src/main/java/com/jenny/medicationreminder/fragment/LoginFragment.java
@@ -0,0 +1,272 @@
+package com.jenny.medicationreminder.fragment;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.jenny.medicationreminder.LoginTelActivity;
+import com.jenny.medicationreminder.LoginUserActivity;
+import com.jenny.medicationreminder.MenuActivity;
+import com.jenny.medicationreminder.Model.User;
+import com.jenny.medicationreminder.R;
+import com.jenny.medicationreminder.RegisterActivity;
+import com.libRG.CustomTextView;
+
+import mehdi.sakout.fancybuttons.FancyButton;
+
+
+public class LoginFragment extends Fragment {
+    FancyButton btnLoginTel, btnLoginUser;
+    EditText etLoginTel, etLoginUser, etLoginPass;
+    CustomTextView tvToRegister;
+    String tel, username, password;
+    FirebaseDatabase database;
+    DatabaseReference loginTelRef, loginUserRef;
+
+    ProgressDialog progressDialog;
+
+    SharedPreferences prefUser;
+    SharedPreferences.Editor editor;
+
+    private static final String USER_PREFS = "userStatus";
+
+    public LoginFragment() {
+        super();
+    }
+
+    public static LoginFragment newInstance() {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        initInstances(rootView);
+        return rootView;
+    }
+
+    private void initInstances(View rootView) {
+        // Init 'View' instance(s) with rootView.findViewById here
+        // Login with phone number
+        btnLoginTel = rootView.findViewById(R.id.btnLoginTel);
+        etLoginTel = rootView.findViewById(R.id.etLoginTel);
+        btnLoginTel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginTel();
+            }
+        });
+
+        // Login with username and password
+        btnLoginUser = rootView.findViewById(R.id.btnLoginUser);
+        etLoginUser = rootView.findViewById(R.id.etLoginUser);
+        etLoginPass = rootView.findViewById(R.id.etLoginPass);
+        btnLoginUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+        // Go to Register
+        tvToRegister = rootView.findViewById(R.id.tvToRegister);
+        tvToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.contentContainer, RegisterFragment.newInstance())
+                        .commit();
+            }
+        });
+    }
+
+    public void loginTel() {
+
+        // Validation form
+        tel = String.valueOf(etLoginTel.getText());
+        if (tel.isEmpty()){
+            etLoginTel.setError("กรุณากรอกเบอร์โทรศัพท์ !");
+            etLoginTel.requestFocus();
+            return;
+        }
+
+        database = FirebaseDatabase.getInstance();
+        loginTelRef = database.getReference("User");
+
+        // Progress Dialog
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("กรุณารอสักครู่");
+        progressDialog.show();
+
+        // Alert Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setCancelable(false);
+        builder.setMessage("หมายเลขโทรศัพท์ไม่ถูกต้อง");
+        builder.setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        final AlertDialog dialog = builder.create();
+
+        // Query database
+        Query query = loginTelRef.orderByChild("User_phone").equalTo(String.valueOf(etLoginTel.getText()));
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                progressDialog.dismiss();
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        User user = snapshot.getValue(User.class);
+                        Log.e("phone", user.getUser_phone());
+                        Log.e("fname", user.getUser_fname());
+                        Log.e("key", String.valueOf(snapshot.getKey()));
+
+                        // Keep user's key and status login in SharedPreferences
+                        prefUser = getActivity().getSharedPreferences(USER_PREFS, Context.MODE_PRIVATE);
+                        editor = prefUser.edit();
+                        editor.putString("keyUser", String.valueOf(snapshot.getKey()));
+                        editor.putBoolean("statusLogin", true);
+                        editor.commit();
+
+                        // Start Menu
+                        Intent intent = new Intent(getActivity(), MenuActivity.class);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }
+                } else {
+                    dialog.show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public void loginUser() {
+        // Validate form
+        username = String.valueOf(etLoginUser.getText());
+        password = String.valueOf(etLoginPass.getText());
+
+        if (username.isEmpty()){
+            etLoginUser.setError("กรุณากรอกชื่อผู้ใช้งาน !");
+            etLoginUser.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            etLoginPass.setError("กรุณากรอกรหัสผ่าน !");
+            etLoginPass.requestFocus();
+            return;
+        }
+
+        database = FirebaseDatabase.getInstance();
+        loginUserRef = database.getReference("User");
+
+        // Alert Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setCancelable(false);
+        builder.setMessage("ชื่อผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง");
+        builder.setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        final AlertDialog dialog = builder.create();
+
+        // Progress Dialog
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("กรุณารอสักครู่");
+        progressDialog.show();
+
+        // Query database
+        loginUserRef.orderByChild("username").equalTo(etLoginUser.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                progressDialog.dismiss();
+                if (dataSnapshot.exists()){
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        User user = snapshot.getValue(User.class);
+                        if (user.getPassword().equals(etLoginPass.getText().toString())){
+
+                            // Keep user's key and status login in SharedPreferences
+                            prefUser = getActivity().getSharedPreferences(USER_PREFS, Context.MODE_PRIVATE);
+                            editor = prefUser.edit();
+                            editor.putString("keyUser", String.valueOf(snapshot.getKey()));
+                            editor.putBoolean("statusLogin", true);
+                            editor.commit();
+
+                            // Start Menu Activity
+                            Intent intent = new Intent(getActivity(), MenuActivity.class);
+                            startActivity(intent);
+                            getActivity().finish();
+                        } else {
+                            dialog.show();
+                        }
+                    }
+                } else {
+                    dialog.show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    /*
+     * Save Instance State Here
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save Instance State here
+    }
+
+    /*
+     * Restore Instance State Here
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            // Restore Instance State here
+        }
+    }
+}
