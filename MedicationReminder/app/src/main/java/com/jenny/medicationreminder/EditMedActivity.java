a784e9b9e6bc2b378f97cdc0d202e2adc8128bc3
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-12-11 02:17:08 +0700

    Change date&time picker dialog and add dialog for custom days. And change style color UI in edit medicine

--- MedicationReminder/app/src/main/java/com/jenny/medicationreminder/EditMedActivity.java
+++ MedicationReminder/app/src/main/java/com/jenny/medicationreminder/EditMedActivity.java
@@ -1,34 +1,45 @@
 package com.jenny.medicationreminder;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
-import android.content.Context;
-import android.graphics.Paint;
+import android.content.DialogInterface;
 import android.graphics.Typeface;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
-import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TimePicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-import junit.runner.Version;
+import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
+import com.codetroopers.betterpickers.radialtimepicker.RadialTimePickerDialogFragment;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
+
+import mehdi.sakout.fancybuttons.FancyButton;
 
 
-public class EditMedActivity extends AppCompatActivity {
+public class EditMedActivity extends AppCompatActivity
+        implements CalendarDatePickerDialogFragment.OnDateSetListener, RadialTimePickerDialogFragment.OnTimeSetListener {
 
     CardView cvAppbar;
-    CheckBox cbMorning, cbLunch, cbEveing, cbBeforeBed;
-    EditText etTimeMorning, etDateStart;
+    CheckBox cbMorning, cbLunch, cbEvening, cbBeforeBed;
+    EditText etTimeMorning, etDateEnd, etMedName;
+    TextView tvTopic;
+    RadioGroup optionAlert;
+    FancyButton btnCustomDays;
+
     private int mYear, mMonth, mDay, mHour, mMinute;
+
+    final String [] daysArr = {"วันอาทิตย์", "วันจันทร์", "วันอังคาร", "วันพุธ", "วันพฤหัสบดี", "วันศุกร์", "วันเสาร์"};
+    final boolean[] checkedDays = new boolean[7];
+
+    private static final String FRAG_TAG_DATE_PICKER = "fragment_date_picker_name";
+    public static final String FRAG_TAG_TIME_PICKER = "fragment_time_picker_name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,58 +55,129 @@ public class EditMedActivity extends AppCompatActivity {
         cvAppbar.setBackgroundResource(R.drawable.bg_appbar);
 
         etTimeMorning = findViewById(R.id.etTimeMorning);
-        etDateStart = findViewById(R.id.etDateStart);
+        etDateEnd = findViewById(R.id.etDateEnd);
+        optionAlert = findViewById(R.id.optionAlert);
+        btnCustomDays = findViewById(R.id.btnCustomDays);
+        btnCustomDays.setEnabled(false);
 
         cbMorning = findViewById(R.id.cbMorning);
         cbLunch = findViewById(R.id.cbLunch);
-        cbEveing = findViewById(R.id.cbEveing);
+        cbEvening = findViewById(R.id.cbEvening);
         cbBeforeBed = findViewById(R.id.cbBeforeBed);
 
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/th_sarabun.ttf");
         cbMorning.setTypeface(typeface, Typeface.BOLD);
         cbLunch.setTypeface(typeface, Typeface.BOLD);
-        cbEveing.setTypeface(typeface, Typeface.BOLD);
+        cbEvening.setTypeface(typeface, Typeface.BOLD);
         cbBeforeBed.setTypeface(typeface, Typeface.BOLD);
 
+        etMedName = findViewById(R.id.etMedName);
+        tvTopic = findViewById(R.id.tvTopic);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            tvTopic.setText(bundle.getString("topic"));
+            etMedName.setText(bundle.getString("medName"));
+        }
 
     }
 
     public void setTime(View view) {
 
-        // Get Current Time
-        final Calendar c = Calendar.getInstance();
-        mHour = c.get(Calendar.HOUR_OF_DAY);
-        mMinute = c.get(Calendar.MINUTE);
+        // Get Date from Edit text
+        String[] timeSplit = etTimeMorning.getText().toString().split(":");
+        mHour = Integer.parseInt(timeSplit[0]);
+        mMinute = Integer.parseInt(timeSplit[1]);
 
-        // Launch Time Picker Dialog
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                etTimeMorning.setText(hourOfDay + ":" + minute);
-            }
-        }, mHour, mMinute, true);
-        timePickerDialog.show();
+        // Show time picker dialog
+        RadialTimePickerDialogFragment rtpd = new RadialTimePickerDialogFragment()
+                .setOnTimeSetListener(EditMedActivity.this)
+                .setStartTime(mHour, mMinute)
+                .setDoneText("ตกลง")
+                .setCancelText("ยกเลิก");
+        rtpd.show(getSupportFragmentManager(), FRAG_TAG_TIME_PICKER);
     }
 
     public void setDate(View view) {
-        // Get Current Date
-        final Calendar c = Calendar.getInstance();
-        mYear = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay = c.get(Calendar.DAY_OF_MONTH);
+        // Get Date from Edit text
+        String[] dateSplit = etDateEnd.getText().toString().split("/");
+        mYear = Integer.parseInt(dateSplit[2]);
+        mMonth = Integer.parseInt(dateSplit[1]);
+        mDay = Integer.parseInt(dateSplit[0]);
 
-        DatePickerDialog datePickerDialog = new DatePickerDialog(EditMedActivity.this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                etDateStart.setText(dayOfMonth + "/" + (month+1) + "/" + year);
-            }
-        }, mYear, mMonth, mDay);
-//        datePickerDialog.setVersion(DatePickerDialog, Version.VERSION_2);
-        datePickerDialog.show();
+        // Show Calendar date picker dialog
+        CalendarDatePickerDialogFragment cdp = new CalendarDatePickerDialogFragment()
+                .setOnDateSetListener(EditMedActivity.this)
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setPreselectedDate(mYear-543, mMonth-1, mDay)
+                .setDoneText("ตกลง")
+                .setCancelText("ยกเลิก");
+        cdp.show(getSupportFragmentManager(), FRAG_TAG_DATE_PICKER);
     }
+
 
 
     public void backBtn(View view) {
         finish();
+    }
+
+    @Override
+    public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth) {
+        etDateEnd.setText(String.format("%02d", dayOfMonth) + "/" + String.format("%02d", monthOfYear+1) + "/" + (year+543));
+    }
+
+    @Override
+    public void onTimeSet(RadialTimePickerDialogFragment dialog, int hourOfDay, int minute) {
+        etTimeMorning.setText(String.format("%02d", hourOfDay) + ":" + String.format("%02d", minute));
+    }
+
+    public void setDays(View view) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(EditMedActivity.this);
+        final List<String> daysList = Arrays.asList(daysArr);
+
+        builder.setTitle("วันที่บริหารยา");
+
+        builder.setMultiChoiceItems(daysArr, checkedDays, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                checkedDays[which] = isChecked;
+                String currentItem = daysList.get(which);
+            }
+        });
+
+        builder.setPositiveButton("ยืนยัน", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+//                String disease = "";
+//                for (int i = 0; i < checkedDays.length; i++) {
+//                    boolean checked = checkedDays[i];
+//                    if (checked) {
+//                        disease += daysList.get(i) + ", ";
+//                    }
+//                }
+//                if (disease.length() > 0) {
+//                    disease = disease.substring(0, disease.length() - 2);
+//                }
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void checkRadioBtn(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.everyAlert:
+                if (checked)
+                    btnCustomDays.setEnabled(false);
+                break;
+            case R.id.customAlert:
+                if (checked)
+                    btnCustomDays.setEnabled(true);
+                break;
+        }
     }
 }
